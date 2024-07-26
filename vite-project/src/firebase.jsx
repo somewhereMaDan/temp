8fb81039d62f139d0fb32ffcfde32b72255432b0
Madan <madanmohan7272906844@gmail.com>
2024-07-26 21:16:21 +0530

    .end

--- vite-project/src/firebase.jsx
+++ vite-project/src/firebase.jsx
@@ -1,29 +1,10 @@
-// import { initializeApp } from "firebase/app";
-// import { getStorage } from "firebase/storage";
-// import { getAnalytics } from "firebase/analytics";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBLXCFAvQej9tJI6HCa_BDzxPx5Qs2jhoE",
-//   authDomain: "pdfextraction-671cd.firebaseapp.com",
-//   projectId: "pdfextraction-671cd",
-//   storageBucket: "pdfextraction-671cd.appspot.com",
-//   messagingSenderId: "459111462789",
-//   appId: "1:459111462789:web:66de143592d9ba39a536ec",
-//   measurementId: "G-P7YW504LMM"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// export const storage = getStorage(app);
-// const analytics = getAnalytics(app);
-
-
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAqXcQ7lTZqD1sHxUNqy_udqTv2oHWF3AA",
+  // apiKey: "AIzaSyAqXcQ7lTZqD1sHxUNqy_udqTv2oHWF3AA",
+  apiKey: `${import.meta.env.FIREBASE_KEY}`,
   authDomain: "fileextraction-c0dfa.firebaseapp.com",
   projectId: "fileextraction-c0dfa",
   storageBucket: "fileextraction-c0dfa.appspot.com",
